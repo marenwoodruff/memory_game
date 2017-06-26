86fd859058326fee0cdbc6c3ec3402f6bf2890fe
MarenWoodruff <wren403@gmail.com>
2017-06-26 18:24:30 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -9,19 +9,38 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+// var cardOne = cards[0];
+// var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-console.log(cardsInPlay);
-cardsInPlay.push(cardTwo);
-console.log(cardsInPlay);
+// cardsInPlay.push(cardOne);
+// console.log("User flipped " + cardOne);
+// cardsInPlay.push(cardTwo);
+// console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert('You found a match!');
-    }
-    else {
-        alert('Sorry, try again.');
+// if (cardsInPlay.length === 2) {
+//     if (cardsInPlay[0] === cardsInPlay[1]) {
+//         alert('You found a match!');
+//     }
+//     else {
+//         alert('Sorry, try again.');
+//     }
+// }
+
+var checkForMatch = function() {
+    if (cardsInPlay.length === 2) { // error in code- 9.6
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log("You found a match!");
+        } else {
+            console.log("Sorry, try again.");
+        }
     }
 }
+
+var flipCard = function(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
