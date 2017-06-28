479250716237960c361f042f14bbbcf7175efbed
MarenWoodruff <wren403@gmail.com>
2017-06-27 21:49:04 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -6,7 +6,29 @@
 // console.log("User flipped " + cardOne);
 // console.log("User flipped " + cardThree);
 
-var cards = ["queen", "queen", "king", "king"];
+// var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 // var cardOne = cards[0];
@@ -37,8 +59,10 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("Card Suit " + cards[cardId].suit);
+    console.log("Card Image " + cards[cardId].cardImage);
     checkForMatch();
 }
 
