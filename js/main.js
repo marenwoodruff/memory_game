87f04529d88cd48eccd6e98655e01fa171d18447
MarenWoodruff <wren403@gmail.com>
2017-06-28 12:05:09 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -52,19 +52,33 @@ var checkForMatch = function() {
     if (cardsInPlay.length === 2) { // error in code- 9.6
         if (cardsInPlay[0] === cardsInPlay[1]) {
             console.log("You found a match!");
+            alert("You found a match!");
         } else {
             console.log("Sorry, try again.");
+            alert("Sorry, try again.");
         }
     }
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     console.log("Card Suit " + cards[cardId].suit);
     console.log("Card Image " + cards[cardId].cardImage);
+    this.setAttribute('src', cards[cardId].cardImage);
     checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+    console.log('placed new cards via JS');
+}
+
+createBoard();
